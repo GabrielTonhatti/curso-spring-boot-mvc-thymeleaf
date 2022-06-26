6d531c5a41b5af71c029896744c9581cc4335364
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-06-26 10:58:59 -0300

    As entidades Departamento e Cargo

---
+++ src/main/java/com/gabrieltonhatti/curso/boot/domain/Endereco.java
@@ -0,0 +1,12 @@
+package com.gabrieltonhatti.curso.boot.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "enderecos")
+public class Endereco extends AbstractEntity<Long> {
+
+
+
+}
