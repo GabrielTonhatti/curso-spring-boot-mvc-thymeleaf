04988b0de269a1b4fedc4d08673056ca6088b3e2
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-06-28 13:29:36 -0300

    Inserindo departamentos

--- src/main/java/com/gabrieltonhatti/curso/boot/web/controller/DepartamentoController.java
+++ src/main/java/com/gabrieltonhatti/curso/boot/web/controller/DepartamentoController.java
@@ -1,12 +1,19 @@
 package com.gabrieltonhatti.curso.boot.web.controller;
 
+import com.gabrieltonhatti.curso.boot.domain.Departamento;
+import com.gabrieltonhatti.curso.boot.service.DepartamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
+
+    @Autowired
+    private DepartamentoService service;
 
     @GetMapping("/listar")
     public String listar() {
@@ -14,8 +21,14 @@ public class DepartamentoController {
     }
 
     @GetMapping("/cadastrar")
-    public String cadastrar() {
+    public String cadastrar(Departamento departamento) {
         return "/departamento/cadastro";
+    }
+
+    @PostMapping("/salvar")
+    public String salvar(Departamento departamento) {
+        service.salvar(departamento);
+        return "redirect:/departamentos/cadastrar";
     }
 
 }
